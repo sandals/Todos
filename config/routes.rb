d5f347749c483e2ef665d53a910a2c7faff3a5cd
Brandon Newton <sandalsboy@gmail.com>
2014-11-15 14:19:38 -0800

    Implemented 'users' and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	root 'todos#index', via: :get
 
+	resource :session, only: [:new, :create]
 	resources :todos, only: [:index, :new, :create]
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,4 +34,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+	# only include the Features module, within the features/ spec
+	config.include Features, type: :feature
 end
