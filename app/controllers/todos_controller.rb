e5335703589ea5de4b16d2c948eab1d4fd2a1fda
Brandon Newton <sandalsboy@gmail.com>
2014-11-15 13:56:00 -0800

    Can add a todo successfully

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,25 @@
 class TodosController < ApplicationController
 	def index
+		@todos = Todo.all
+	end
+
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		
+		if @todo.save
+			redirect_to root_path
+		else
+			redirect_to root_path, error: "could not save todo"
+		end
+	end
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:title)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 	root 'todos#index', via: :get
+
+	resources :todos, only: [:new, :create]
 end
