877d9f7a2a15471009e63c10950abfd01e7ef02a
Brandon Newton <sandalsboy@gmail.com>
2014-11-16 10:10:25 -0800

    Spec for #incomplete

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -24,3 +24,14 @@ describe Todo, "#complete!" do
 		expect(todo).to be_completed
 	end
 end
+
+describe Todo, "#incomplete!" do
+	it "sets completed_at to nil" do
+		todo = Todo.create!(completed_at: Time.current)
+		
+		todo.incomplete!
+		todo.reload
+
+		expect(todo).not_to be_completed
+	end
+end
