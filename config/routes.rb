a4637616de5057e31a2ab516d6dfc4a6f1b684f6
Brandon Newton <sandalsboy@gmail.com>
2014-11-16 09:31:31 -0800

    Allow todos to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 	root 'todos#index', via: :get
 
 	resource :session, only: [:new, :create]
-	resources :todos, only: [:index, :new, :create]
+	resources :todos, only: [:index, :new, :create] do
+		resource :completion, only: [:create]
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115222112) do
+ActiveRecord::Schema.define(version: 20141116172624) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.datetime "completed_at"
   end
 
 end
